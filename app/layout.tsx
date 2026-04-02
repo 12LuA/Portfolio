e3e9b76532ebc82756638b78c3d254141a94e9d7
12LuA <71656192+12LuA@users.noreply.github.com>
2026-04-02 13:57:36 +0200

    feat: update color variables and add background gradient to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
       className={cn("antialiased", fontMono.variable, "font-sans", geist.variable)}
     >
       <body>
-        <ThemeProvider>{children}</ThemeProvider>
+        <ThemeProvider>{children} <div className='background-gradient' /></ThemeProvider>
       </body>
     </html>
   )
